63c8bca2b1eca92484c7f5ec20021b404562a6ed
KaushikBaidya <47146123+KaushikBaidya@users.noreply.github.com>
2022-02-03 14:23:55 +0600

    portfolio project

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,31 @@
+import { Topbar } from '../topbar/Topbar'
+import './intro.css'
+
+export const Intro = () => {
+  return (
+    <div className="container">
+      <Topbar />
+      <div className="content">
+        <div className="intro">Hello! I'm Kaushik Baidya</div>
+        <div className="title">
+          I am a{' '}
+          <span style={{ color: 'tomato', fontWeight: '500' }}>
+            WEB DEVELOPER
+          </span>
+        </div>
+        <div className="desc">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis,
+          atque sit dolore quia placeat adipisci repudiandae maxime deserunt.
+          Error, itaque possimus dicta mollitia dolores modi sequi fuga ipsam.
+          Saepe modi nostrum dolores ullam quo quasi voluptatum blanditiis
+          distinctio? Mollitia sit deleniti dolorum? Quas perspiciatis voluptate
+          asperiores labore? Corporis, suscipit! Sapiente!
+        </div>
+        <button className="btn">
+          {' '}
+          <span>Contact ME</span>{' '}
+        </button>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,56 @@
+import './topbar.css'
+import { FaBars } from 'react-icons/fa'
+export const Topbar = () => {
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <div className="logo">
+            <i className="fas fa-dice-six" style={{ fontSize: '40px' }}></i>
+          </div>
+          <button className="nav-toggle">
+            <FaBars />
+          </button>
+        </div>
+        <div className="links-container show-container">
+          <ul className="links">
+            <li>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contact-Me</li>
+          </ul>
+        </div>
+        <ul className="social-icons">
+          <li>
+            <a href="https://www.facebook.com/kaushik.baidya.161">
+              <i class="fab fa-facebook-square"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/KaushikBaidya">
+              <i class="fab fa-github-alt"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/kaushik-baidya-696247157/">
+              <i class="fab fa-linkedin"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    // <div className="top-bar">
+    //   <div className="logo">
+    //     <i className="fas fa-dice-six" style={{ color: 'tomato' }}></i>
+    //   </div>
+    //   <div className="nav-bar">
+    //     <ul>
+    //       <li className="active">Home</li>
+    //       <li>About</li>
+    //       <li>Projects</li>
+    //       <li>Contact</li>
+    //     </ul>
+    //   </div>
+    // </div>
+  )
+}
